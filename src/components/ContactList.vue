bb0383e5474f9b61cfae5bb5b2c7b42dd2d40ba2
TranLeMinhB2014759 <minhb2014759@student.ctu.edu.vn>
2023-10-26 11:25:42 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/ContactList.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    props: {
+        contacts: { type: Array, deafault: [] },
+        activeIndex: { type: Number, deafault: -1},
+    },
+    emits: ["update:activeIndex"],
+    methods: {
+        updateActiveIndex(index)  {
+            this.$emit("update:activeIndex", index);
+        }
+    }
+};
+</script>
+
+<template>
+    <ul class="list-group">
+        <li 
+            class="list-group-item"
+            v-for="(contact, index) in contacts"
+            :key="index"
+            :class="{active: index === activeIndex}"
+            @click="updateActiveIndex(index)"
+        >
+        {{  contact.name }}
+        </li>
+    </ul>
+</template>
