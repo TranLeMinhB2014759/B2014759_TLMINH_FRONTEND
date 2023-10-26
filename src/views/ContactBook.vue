685cd69d732280383de447af5e830aca8b1c09a5
TranLeMinhB2014759 <minhb2014759@student.ctu.edu.vn>
2023-10-26 11:56:56 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -115,6 +115,16 @@ export default {
           Chi tiết liên hệ
           <i class="fas fa-address-card"></i>
           <ContactCard :contact="activeContact" />
+          <router-link
+            :to="{
+              name: 'contact.edit',
+              params: { id: activeContact._id },
+            }"
+          >
+            <span class="mt-2 badge badge-warning">
+              <i class="fas fa-edit"></i> Hiệu chỉnh
+            </span>
+          </router-link> 
         </h4>
       </div>
     </div>
